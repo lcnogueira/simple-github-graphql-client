8b4afc3687b4b4196ede109d98370be126656176
Luiz Cláudio <luizcns18@gmail.com>
2019-01-16 00:22:55 -0300

    feat: Optimistic mutations

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -20,6 +20,38 @@ const VIEWER_SUBSCRIPTIONS = {
 const isWatch = viewerSubscription =>
   viewerSubscription === VIEWER_SUBSCRIPTIONS.SUBSCRIBED;
 
+const updateWatch = (
+  client,
+  {
+    data: {
+      updateSubscription: {
+        subscribable: { id, viewerSubscription },
+      },
+    },
+  },
+) => {
+  const repository = client.readFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+  });
+  let { totalCount } = repository.watchers;
+  totalCount =
+    viewerSubscription === VIEWER_SUBSCRIPTIONS.SUBSCRIBED
+      ? totalCount + 1
+      : totalCount - 1;
+  client.writeFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+    data: {
+      ...repository,
+      watchers: {
+        ...repository.watchers,
+        totalCount,
+      },
+    },
+  });
+};
+
 const updateAddStar = (
   client,
   {
@@ -99,11 +131,23 @@ const RepositoryItem = ({
             ? VIEWER_SUBSCRIPTIONS.UNSUBSCRIBED
             : VIEWER_SUBSCRIPTIONS.SUBSCRIBED,
         }}
+        optimisticResponse={{
+          updateSubscription: {
+            __typename: 'Mutation',
+            subscribable: {
+              __typename: 'Repository',
+              id,
+              viewerSubscription: isWatch(viewerSubscription)
+                ? VIEWER_SUBSCRIPTIONS.UNSUBSCRIBED
+                : VIEWER_SUBSCRIPTIONS.SUBSCRIBED,
+            },
+          },
+        }}
+        update={updateWatch}
       >
         {(updateSubscription, { data, loading, error }) => (
           <Button
             className="RepositoryItem-title-action"
-            data-test-id="updateSubscription"
             onClick={updateSubscription}
           >
             {watchers.totalCount}{' '}
@@ -116,6 +160,16 @@ const RepositoryItem = ({
         <Mutation
           mutation={STAR_REPOSITORY}
           variables={{ id }}
+          optimisticResponse={{
+            addStar: {
+              __typename: 'Mutation',
+              starrable: {
+                __typename: 'Repository',
+                id,
+                viewerHasStarred: !viewerHasStarred,
+              },
+            },
+          }}
           update={updateAddStar}
         >
           {(addStar, { data, loading, error }) => (
@@ -131,6 +185,16 @@ const RepositoryItem = ({
         <Mutation
           mutation={UNSTAR_REPOSITORY}
           variables={{ id }}
+          optimisticResponse={{
+            removeStar: {
+              __typename: 'Mutation',
+              starrable: {
+                __typename: 'Repository',
+                id,
+                viewerHasStarred: !viewerHasStarred,
+              },
+            },
+          }}
           update={updateRemoveStar}
         >
           {(removeStar, { data, loading, error }) => (
